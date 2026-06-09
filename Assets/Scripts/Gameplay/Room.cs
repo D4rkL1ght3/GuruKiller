952e471735decf9330d44df4f1a2f204f5847fbf
D4rkL1ght3 <farrellimpirson@gmail.com>
2026-06-10 01:25:54 +0700

    Merge complete

--- Assets/Scripts/Gameplay/Room.cs
+++ Assets/Scripts/Gameplay/Room.cs
@@ -2,9 +2,22 @@ using UnityEngine;
 
 public class Room : MonoBehaviour
 {
+    public enum RoomType
+    {
+        Hallway,
+        Classroom,
+        Other
+    }
+
+    [Header("Room Info")]
+    public RoomType roomType;
+
     [Header("Player Bounds")]
     public Vector2 minBounds;
     public Vector2 maxBounds;
+
+    [Header("Teacher")]
+    public Transform[] teacherPatrolPoints;
 
     public void ShowRoom()
     {
